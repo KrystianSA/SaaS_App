926f331f33c775ae33a947953a9b169cddbdac86
KrystianSA <122701689+KrystianSA@users.noreply.github.com>
2024-03-28 16:46:37 +0100

    New table in database to store reset password tokens

---
+++ Database/SaaS_App.SqlDatabase/dbo/Tables/Tokens.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tokens]
+(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[UserId] varchar(36) NOT NULL,
+	[Token] VARCHAR(128) NOT NULL unique,
+	[Token_Expiry] DateTime not null,
+	CONSTRAINT [PK_UserId_Token] PRIMARY KEY ([UserId],[Token] ASC)
+)

36a3d055050609f7bde7adb1c76a331cefec23c9
KrystianSA <122701689+KrystianSA@users.noreply.github.com>
2024-04-01 13:04:12 +0200

    Increase database about IsActive kolumn

--- Database/SaaS_App.SqlDatabase/dbo/Tables/Accounts.sql
+++ Database/SaaS_App.SqlDatabase/dbo/Tables/Accounts.sql
@@ -3,5 +3,6 @@
 	[Id] INT IDENTITY(1,1) NOT NULL, 
     [Name] NCHAR(100) NOT NULL, 
     [CreateDate] DATETIMEOFFSET NOT NULL, 
+    [IsActive] Bit NOT NULL, 
     CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC) 
 )

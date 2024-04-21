6864a04a2542a20f04181042ba9207d87e7b7b61
KrystianSA <122701689+KrystianSA@users.noreply.github.com>
2024-04-21 12:23:46 +0200

    Page for add Url to trace and validation rule for Url

---
+++ Frontend/pages/add-feed.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="d-flex justify-center mt-10">
+        <VCard width="600">
+            <VForm :disabled="loading" @submit.prevent="submit">
+                <VCardTitle class="text-center">New Url</VCardTitle>
+                <VCardText>
+                    <VTextField :rules="[ruleRequired,ruleUrl]" variant="solo-filled" label="Url"
+                        v-model="modelData.url"></VTextField>
+                </VCardText>
+                <VCardActions>
+                    <VBtn :loading="loading" class="mx-auto" variant="elevated" text="Submit" type="submit">
+                    </VBtn>
+                </VCardActions>
+            </VForm>
+        </VCard>
+    </div>
+</template>
+
+<script setup>
+const { ruleRequired, ruleUrl } = useFormValidationRules();
+const loading = ref(false);
+const modelData = ref({
+    url: ''
+});
+const submit = async (ev) => {
+    const { valid } = await ev;
+    if (valid) {
+    }
+}
+</script>
--- Frontend/utils/formValidationRules.js
+++ Frontend/utils/formValidationRules.js
@@ -10,6 +10,11 @@ export const useFormValidationRules = () => {
 			const pattern =
 				/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,}$/;
 			return pattern.test(value) || "Password must contain one digit from 1 to 9, one lowercase letter, one uppercase letter, one special character, no space, and it must be minimum 8 characters long";
+		},
+		ruleUrl: (value) => {
+			const pattern =
+				/^(https?|ftps?):\/\/(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}(?::(?:0|[1-9]\d{0,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?(?:\/(?:[-a-zA-Z0-9@%_\+.~#?&=]+\/?)*)?$/;
+			return pattern.test(value) || "Invalid Url";
 		}
 	};
 };

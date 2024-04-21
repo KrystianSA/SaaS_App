c766f97cff342b68a6cacd92f0fe26db73c4a156
KrystianSA <122701689+KrystianSA@users.noreply.github.com>
2024-04-21 12:03:14 +0200

    Read and unread post, new style for link

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -10,7 +10,7 @@
                     <VCard>
                         <VList>
                             <VListItem @click="deleteItems(items.length)" title="Delete All"></VListItem>
-                            <VListItem title="All Post Read"></VListItem>
+                            <VListItem @click="readPost(items.length)" title="All Post Read"></VListItem>
                         </VList>
                     </VCard>
                 </VMenu>
@@ -19,13 +19,15 @@
                     hide-details single-line></v-text-field>
             </v-card-title>
             <VList v-for="(item, index) in filteredItems" :key="index" class="mx-5">
-                <VCard>
+                <VCard :color="item.color">
                     <template v-slot:prepend>
-                        <VCheckbox @click="changeColor(index)" :model-value="item.checked"></VCheckbox>
+                        <VCheckbox :model-value="item.checked"></VCheckbox>
                     </template>
                     <template v-slot:title>{{ item.title }}</template>
                     <template v-slot:subtitle>
-                        <a :href="item.url" target="_blank">{{ item.url }}</a></template>
+                        <a :href="item.url" target="_blank" @click="readPost(index)">{{ item.url }}</a>
+                        <VIcon :icon=mdiOpenInNew class="ml-1"></VIcon>
+                    </template>
                     <template v-slot:append>
                         <VBtn :icon=mdiDelete @click="deleteItems(index)"></VBtn>
                     </template>
@@ -35,8 +37,36 @@
     </div>
 </template>
 
+<style>
+a {
+    text-decoration: none;
+    color: black
+}
+</style>
+
 <script setup>
-import { mdiDelete, mdiMagnify } from '@mdi/js';
+import { mdiDelete, mdiMagnify, mdiOpenInNew } from '@mdi/js';
+
+const checkboxState = ref(false);
+
+onMounted(() => {
+    readFeed();
+});
+
+const readPost = (index) => {
+    if (items.value.length == index) {
+        items.value.forEach(item => {
+            item.color = 'white';
+            item.checked = false;
+        })
+    }
+    else {
+        var element = items.value[index];
+        element.color = 'white';
+    }
+
+    checkboxState.value = false;
+}
 
 const deleteItems = (itemIndex) => {
     if (items.value.length == itemIndex) {
@@ -45,9 +75,9 @@ const deleteItems = (itemIndex) => {
     else {
         items.value.splice(itemIndex, 1)
     }
+    checkboxState.value = false;
 }
 
-const checkboxState = ref(false);
 const changeAll = () => {
     if (checkboxState.value) {
         items.value.forEach(item => {
@@ -61,10 +91,6 @@ const changeAll = () => {
     }
 }
 
-onMounted(() => {
-    readFeed();
-});
-
 const filteredItems = computed(() => {
     return items.value.filter(item => {
         return item.title.toLowerCase().includes(search.value.toLowerCase());
@@ -74,22 +100,22 @@ const filteredItems = computed(() => {
 const search = ref('');
 const items = ref([
     {
-        index: 1,
         title: 'How to be safe in Network?',
         url: 'https://example.pl',
         checked: false,
+        color: 'red',
     },
     {
-        index: 2,
         title: 'The best antimalware in 2024 !',
         url: 'https://example1.pl',
         checked: false,
+        color: 'red',
     },
     {
-        index: 3,
         title: 'Password Manager, is it safe?',
         url: 'https://example.pl',
         checked: false,
+        color: 'red',
     },
 ]);
 

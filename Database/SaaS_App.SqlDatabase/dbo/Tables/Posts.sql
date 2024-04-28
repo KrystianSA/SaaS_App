516559c35ce462765a5affe26b61a617ed165104
KrystianSA <122701689+KrystianSA@users.noreply.github.com>
2024-04-28 17:58:16 +0200

    Table UrlFeeds and Posts

---
+++ Database/SaaS_App.SqlDatabase/dbo/Tables/Posts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Posts]
+(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Title] VARCHAR(MAX) NOT NULL,
+	[Url] VARCHAR(MAX) NOT NULL,
+	[UrlFeedId] int NOT NULL
+	CONSTRAINT [PK_Post] PRIMARY KEY ([Id] ASC), 
+    CONSTRAINT [FK_Post_UrlFeed] FOREIGN KEY ([UrlFeedId]) REFERENCES [dbo].[UrlFeeds]([Id])
+)
---
+++ Database/SaaS_App.SqlDatabase/dbo/Tables/UrlFeeds.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UrlFeeds]
+(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Url] VARCHAR(MAX) NOT NULL,
+	[AccountId] int NOT NULL,
+	CONSTRAINT [PK_UrlFeed] PRIMARY KEY ([Id] ASC), 
+    CONSTRAINT [FK_UrlFeed_Account] FOREIGN KEY ([AccountId]) REFERENCES [dbo].[Accounts]([Id])
+)
